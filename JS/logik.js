ce01b0e600eff4519a652a3f54bf8010379a5f31
Shimansky <shima3d@gmail.com>
2019-08-14 13:35:19 +0300

    Changes: new CSS colors & buttons, js-function changed

--- JS/logik.js
+++ JS/logik.js
@@ -9,7 +9,23 @@ function showFormCandidate(){
 var header = document.getElementById('header');
 var div = document.createElement("div");
 div.id = "candidateForm";
-div.innerHTML = "<form action='send.php' method='post' id='sendForm'>Заполните форму <br><br><br>Ваше имя: <br><input type='text' name='first_name'><br><br>Email: <br><input type='text' name='email'required placeholder='Обязательное поле'><br><br>Телефон: <br><input type='text' name='phone'><br><br>Сообщение:<br><input type='text'  name='message' ><br><br><input id ='redButton' type='submit' name='submit' value='Отправить'><button id='close' onclick='exitForm()'>ОТМЕНА</button></form>";
+div.innerHTML = "<form action='send.php' method='post' id='sendForm'>Пожалуйста аккуратно заполните все поля <br><br><br>Имя: <br><input type='text' name='first_name' required placeholder='Имя'><br><br>E-mail: <br><input type='text' name='email'required placeholder='E-mail'><br><br>Телефон: <br><input type='text' name='phone' required placeholder='телефон'><br><br>Язык программирования: <br><select><option>Java</option><option>JavaScript</option><option>C#</option><option> Kotlin</option><option>Rust</option><option>Python</option><option>C++</option><option>PHP</option><option>Go</option><option> Swift</option></select><br><br>Сообщение:<br><input type='text'  name='message' ><br><br><input id ='sendButton' type='submit' name='submit' value='Отправить'><button id='dissalowButton' onclick='exitForm()'>ОТМЕНА</button></form>";
+header.appendChild(div);
+button.style.display = "none";
+anotherButton.style.display = "none";
+logotip.style.display = "none";
+card.style.display = "none";
+hr.style.display = "none";
+}
+
+
+
+
+function showFormClient(){
+var header = document.getElementById('header');
+var div = document.createElement("div");
+div.id = "candidateForm";
+div.innerHTML = "<form action='send.php' method='post' id='sendForm'>Заполните форму <br><br><br>Ваше имя: <br><input type='text' name='first_name'><br><br>Email: <br><input type='text' name='email'required placeholder='Обязательное поле'><br><br>Телефон: <br><input type='text' name='phone'><br><br>Сообщение:<br><input type='text'  name='message' ><br><br><input id ='sendButton' type='submit' name='submit' value='Отправить'><button id='dissalowButton' onclick='exitForm()'>ОТМЕНА</button></form>";
 header.appendChild(div);
 button.style.display = "none";
 anotherButton.style.display = "none";
